c508528e73a922a67b7ace9587f7fdaa10ad4c13
manthanbari98 <bari.manthan98@gmail.com>
2026-08-16 19:58:05 +0530

    Add audit schema and ETL log table creation
    Create audit schema and ETL log table for batch processing.

---
+++ notebooks/audit_tables/audit.py
@@ -0,0 +1,27 @@
+#CREATE AUDIT SCHEMA AND TABLE
+
+spark.sql("""
+          CREATE SCHEMA IF NOT EXISTS batch_process.audit;
+          """)
+
+# COMMAND ----------
+
+spark.sql("""
+CREATE TABLE IF NOT EXISTS batch_process.audit.etl_log
+(
+    log_id BIGINT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
+    run_id STRING,
+    pipeline_name STRING,
+    table_name STRING,
+    layer STRING,
+    start_time TIMESTAMP,
+    end_time TIMESTAMP,
+    duration_seconds DOUBLE,
+    rows_read INT,
+    rows_inserted INT,
+    rows_updated INT,
+    rows_deleted INT,
+    status STRING,
+    error_message STRING
+)
+""")
