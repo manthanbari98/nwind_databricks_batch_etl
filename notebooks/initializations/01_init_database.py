c97632781129dd975c8a91032c37c19d91346e2e
manthanbari98 <bari.manthan98@gmail.com>
2026-08-16 19:55:27 +0530

    Create Batch_Process catalog and schemas
    This script initializes a database catalog and its schemas.

---
+++ notebooks/initializations/01_init_database.py
@@ -0,0 +1,12 @@
+# SCRIPT PURPOSE : This script creates a Catalog 'Batch_Process' after checking if it exists already.
+#	           The script also creates three schemas : bronze, silver, gold.
+
+catalog_name = "Batch_Process"
+
+# Drop if exists
+spark.sql(f"CREATE CATALOG IF NOT EXISTS {catalog_name}")
+
+# Create schemas
+spark.sql(f"CREATE SCHEMA IF NOT EXISTS {catalog_name}.bronze")
+spark.sql(f"CREATE SCHEMA IF NOT EXISTS {catalog_name}.silver")
+spark.sql(f"CREATE SCHEMA IF NOT EXISTS {catalog_name}.gold")
